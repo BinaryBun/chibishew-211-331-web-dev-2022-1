3a02c0ee745f1d45f422c96331c6321c76239a6c
BinaryBun <chibishew2016@yandex.ru>
2023-01-16 22:48:49 +0300

    final

--- project/LK/modal.js
+++ project/LK/modal.js
@@ -26,12 +26,26 @@ for (var i = 0; i < edit.length; i++) {
         var guid = modal.getElementsByClassName("guid-name")[0];
         var routr = modal.getElementsByClassName("route-name")[0];
         var price = modal.getElementsByClassName("price")[0];
+        var date = modal.getElementsByClassName("date-input")[0];
+        var time = modal.getElementsByClassName("time-input")[0];
+        var persons = modal.getElementsByClassName("peoples-input")[0];
+        var timeCount = modal.getElementsByClassName("count-time-input")[0];
+        var op1 = modal.getElementsByClassName("opt-1")[0];
+        var op2 = modal.getElementsByClassName("opt-2")[0];
+        var price = modal.getElementsByClassName("price")[0];
 
-        console.log(this.parentNode.getElementsByClassName("guid-name")[0]);
         guid.innerHTML = this.parentNode.getElementsByClassName("guid-name")[0].innerHTML.replaceAll("+", " ");
         routr.innerHTML = this.parentNode.getElementsByClassName("route-name")[0].innerHTML;
         price.innerHTML = this.parentNode.getElementsByClassName("price")[0].innerHTML;
+        date.value = this.parentNode.getElementsByClassName("date")[0].innerHTML;
+        time.value = this.parentNode.getElementsByClassName("time")[0].innerHTML;
+        persons.value = this.parentNode.getElementsByClassName("persons")[0].innerHTML;
+        timeCount.value = this.parentNode.getElementsByClassName("duration")[0].innerHTML;
+        op1.checked = (this.parentNode.getElementsByClassName("optionFirst")[0].innerHTML === 'true');
+        op2.checked = (this.parentNode.getElementsByClassName("optionSecond")[0].innerHTML === 'true');
         modal.style.display = "flex";
+
+        getPrice(this.parentNode.getElementsByClassName("start_price")[0].innerHTML, modal)
     }
 }
 
@@ -68,12 +82,13 @@ for (var i = 0; i < send.length; i++) {
                 "date": date,
                 "time": time,
                 "guide_id": guid,
-                "counTime": counTime,
+                "duration": counTime,
                 "persons": peoples,
                 "option_first": op1.toString(),
                 "option_second": op2.toString(),
                 "route_id": routeName,
-                "price": price
+                "price": price,
+                "student_id": mainDiv.getElementsByClassName("price")[0].value
             })
         })
         .then(function() {
@@ -126,6 +141,7 @@ function getPrice(startPrice, mainDiv) {
             time = 0;
         }
 
+        mainDiv.getElementsByClassName("price")[0].value = startPrice;
         mainDiv.getElementsByClassName("price")[0].innerHTML = startPrice * day * hours + persons + time;
     }
     mainDiv.getElementsByClassName("count-time-input")[0].oninput = template;
